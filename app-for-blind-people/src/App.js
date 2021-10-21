833e93c1ceac02883d365463f55a58450b27e6a6
Enrique Vásconez <vasconez111@hotmail.com>
2021-10-21 16:42:15 -0500

    Test
    Prueba de conexión fontend con api

--- app-for-blind-people/src/App.js
+++ app-for-blind-people/src/App.js
@@ -13,37 +13,51 @@ import Register from './Components/register';
 import SiderBar from './Components/sidebar';
 import { useState } from 'react';
 
+async function rest (){
+let response = await fetch('https://blind-people-app-backend.herokuapp.com/user');
+if (response.ok) {
+  let json = await response.json();
+  console.log(json)
+} else {
+  alert("Error-HTTP: " + response.status);
+}
+}
 
+ function App () {
 
-function App() {
+ 
 
   return (
-    <Router> 
-    <div className="App">
-   
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <Switch>
-               <Route exact path ="/register" >
-                    <Register/>
-                    
-                </Route>
-                <Route exact path ="/" >
-                   
-                </Route>
-              <Route exact path ="/login">
-                
-                <Login/>
+    <Router>
+      <div className="App">
 
-              </Route>
-              
-            </Switch>
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <Switch>
+          <Route exact path="/register" >
+            <Register />
+
+          </Route>
+          <Route exact path="/" >
+ 
+          </Route>
+          <Route exact path="/login">
+
+            <Login />
+            <button onClick = {rest} > Boton </button>
+
+            
+          </Route>
          
-          {/* <Footer/> */}
-           
+        </Switch>
+      
+
+        {/* <Footer/> */}
+       
       </div>
     </Router>
+    
 
   );
 }
--- app-for-blind-people/src/Components/register.js
+++ app-for-blind-people/src/Components/register.js
@@ -1,46 +1,49 @@
 import React from "react";
 import 'C:/Users/USER/Desktop/Universidad/App_Tesis/App_for_blind_people/app-for-blind-people/src/App.css'
 const Register = () => {
+  
+ const handleSubmit = e =>{
+   e.preventDefault();
+    console.log('Hellow this works :)')
+  }
     return (
-
-      <body>
-      <section class="vh-100   gradient-custom">
-        <div id="container" class= "container py-5 h-100">
-          <div class="row justify-content-center align-items-center h-100">
-            <div class="col-12 col-lg-9 col-xl-7">
-              <div id="card" class="card shadow-2-strong card-registration" >
-                <div class="card-body p-4 p-md-5">
-                  <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Llenar campos</h3>
+ 
+      <section onSubmit={handleSubmit} className="vh-100   gradient-custom">
+        <div id="container" className= "container py-5 h-100">
+          <div className="row justify-content-center align-items-center h-100">
+            <div className="col-12 col-lg-9 col-xl-7">
+              <div id="card" className="card shadow-2-strong card-registration" >
+                <div className="card-body p-4 p-md-5">
+                  <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">Llenar campos</h3>
                   <form>
       
-                    <div class="row">
-                      <div class="form-outlin mb-4">
+                    <div className="row">
+                      <div className="form-outlin mb-4">
       
-                        <div class="form-outline">
-                        <label class="form-label" for="firstName"> Nombre</label>
-                          <input type="text" id="firstName" placeholder="First name" class="form-control form-control-lg" />
-                      
-                        </div>
+                        <div className="form-outline">
+                        <label className="form-label" htmlFor="firstName"> Nombre</label>
+                          <input type="text" id="firstName" placeholder="First name" className="form-control form-control-lg" />
+                                            </div>
       
                       </div>
                      
                     </div>
       
-                    <div class="row">
-                      <div class="col-md-6 mb-4 ">
+                    <div className="row">
+                      <div className="col-md-6 mb-4 ">
       
-                        <div class="form-outline w-100">
-                        <label class="form-label" for="emailAddress">Correo</label>
-                        <input type="email" id="emailAddress" class="form-control form-control-lg" />
+                        <div className="form-outline w-100">
+                        <label className="form-label" htmlFor="emailAddress">Correo</label>
+                        <input type="email" id="emailAddress" className="form-control form-control-lg" />
                           
                         </div>
       
                       </div>
-                      <div class="col-md-6 mb-4 pb-2">
+                      <div className="col-md-6 mb-4 pb-2">
 
-                        <div class="form-outline">
-                            <label class="form-label" for="phoneNumber">Teléfono</label>
-                            <input type="tel" id="emailAddress" class="form-control form-control-lg" />
+                        <div className="form-outline">
+                            <label className="form-label" htmlFor="phoneNumber">Teléfono</label>
+                            <input type="tel" id="emailAddress" className="form-control form-control-lg" />
 
                         </div>
 
@@ -48,39 +51,39 @@ const Register = () => {
                    
                     </div>
       
-                    <div class="row">
-                      <div class="col-md-6 mb-4 pb-2">
+                    <div className="row">
+                      <div className="col-md-6 mb-4 pb-2">
       
-                        <div class="form-outline">
-                          <label class="form-label" for="password">Contraseña</label>
-                          <input type="password" id="password" class="form-control form-control-lg" />
+                        <div className="form-outline">
+                          <label className="form-label" htmlFor="password">Contraseña</label>
+                          <input type="password" id="password" className="form-control form-control-lg" />
                           
                         </div>
       
                       </div>
-                      <div class="col-md-6 mb-4 pb-2">
-                        <div class="form-outline">
-                          <label class="form-label" for="password">Repetir Contraseña</label>
-                          <input type="password" id="password" class="form-control form-control-lg" /> 
+                      <div className="col-md-6 mb-4 pb-2">
+                        <div className="form-outline">
+                          <label className="form-label" htmlFor="password">Repetir Contraseña</label>
+                          <input type="password" id="password" className="form-control form-control-lg" /> 
                         </div>
       
                       </div>
                     </div>
       
-                    <div class="row">
-                      <div class="col-md-6 mb-4 pb-2">
+                    <div className="row">
+                      <div className="col-md-6 mb-4 pb-2">
       
-                      <div class="form-outline">
-                          <label class="form-label" for="PorcentajeDiscapacidad">Porcentaje de Discapacidad</label>
-                          <input type="number" id="PorcentajeDiscapacidad" class="form-control form-control-lg" />
+                      <div className="form-outline">
+                          <label className="form-label" htmlFor="PorcentajeDiscapacidad">Porcentaje de Discapacidad</label>
+                          <input type="number" id="PorcentajeDiscapacidad" className="form-control form-control-lg" />
                         </div>
                       </div>
-                      <div class="col-md-6 mb-4 pb-2">
+                      <div className="col-md-6 mb-4 pb-2">
       
-                        <div class="form-outline">
+                        <div className="form-outline">
                             <br/>
                             
-                        <input id="Submit" class=" container btn btn-primary btn-lg" type="submit" value="Crear" />
+                        <input id="Submit" className=" container btn btn-primary btn-lg" type="submit" value="Crear" />
                             </div>
                         </div>
 
@@ -94,8 +97,7 @@ const Register = () => {
         </div>
       
       </section>
-      </body>
-      
+ 
     );
 }
 export default Register;
