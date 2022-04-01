67c2e580a4b7115d2b41a9a2dcd6c0836e6aed06
enriquevasconez <vasconez111@hotmail.com>
2022-04-01 14:46:20 -0500

    StarRating
    Sistema de calificación

--- src/Components/pages/serviceDetail/serviceDetail.js
+++ src/Components/pages/serviceDetail/serviceDetail.js
@@ -174,8 +174,8 @@ const ServiceDetail = () => {
                                 <p><b>Categoría: </b>{`${service?.sc?.sc_name}`}</p>
 
                             </div>
-
-                            <div className="col">
+                            
+                            <div className="col ">
                                <Star   
                                serviceID ={service_id}/>
                             </div>
