5372a11030ea658ef8c8b44c59984dde123aae2b
enriquevasconez <vasconez111@hotmail.com>
2022-04-01 12:25:05 -0500

    Update demand card

--- src/Components/pages/serviceDetail/demandService.js
+++ src/Components/pages/serviceDetail/demandService.js
@@ -7,18 +7,18 @@ const Demand = ({ demand }) => {
     return (
         
         <section role="Comentarios de usuarios" className="mt-2">
-            <h4>Comentarios</h4>
+            
             {
                 demand?.map(
                     (element, key) =>
 
-                        <div key={key} className="card mt-2 bg-secondary">
+                        <div key={key} className="card mt-2 bg-success">
                             <div>
                                 <div className="card-body">
                                     <h5 class="card-text mt-2">{element.demand_title}</h5>
                                     <p class="card-text mt-2"> {element.demand_description} </p>
-                                    <p class="card-text mt-2">  {element.user?.user_name}  </p>
-                                    <p class="card-text mt-2">  {new Date(element.createdDate).toLocaleDateString().toString()}  </p>
+                                    <p class="card-text mt-2"> <b>Publicado  por:</b> {element.user?.user_name}  </p>
+                                    <p class="card-text mt-2"> <b>Publicado el:</b> {new Date(element.createdDate).toLocaleDateString().toString()}  </p>
                                 </div>
                               
                             </div>
